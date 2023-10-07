902fec7cdd8070b86ea2a74cce82f0547d321754
Alvaro Clavijo <clavibaldi@gmail.com>
2023-10-07 15:49:45 -0400

    [THF-2] Commits table > Add styles

--- client/src/components/CommitTable/index.tsx
+++ client/src/components/CommitTable/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTable, HeaderGroup, Cell, Row, Column } from "react-table";
 import { Commit } from "../../types/common/Commit";
+import classes from "./styles.module.css";
 
 interface CommitTableProps {
   data: Commit[];
@@ -11,20 +12,52 @@ const CommitTable: React.FC<CommitTableProps> = ({ data }) => {
     () => [
       {
         Header: "Author",
-        accessor: "commit.author.name"
+        accessor: "commit.author.name",
+        Cell: ({ value }) => (
+          <p
+            className={classes["single-line"]}
+            style={{ width: "clamp(100px, 20vw, 200px" }}
+          >
+            {value}
+          </p>
+        ),
       },
       {
         Header: "Commit",
         accessor: "sha",
+        Cell: ({ value }) => (
+          <p
+            className={classes["single-line"]}
+            style={{ width: "clamp(100px, 20vw, 200px" }}
+          >
+            {value}
+          </p>
+        ),
       },
       {
         Header: "Message",
         accessor: "commit.message",
+        Cell: ({ value }) => (
+          <p
+            className={classes["single-line"]}
+            style={{ width: "clamp(300px, 30vw, 550px" }}
+          >
+            {value}
+          </p>
+        ),
       },
       {
         Header: "Date",
-        accessor: "commit.author.date"
-      }
+        accessor: "commit.author.date",
+        Cell: ({ value }) => (
+          <p
+            className={classes["single-line"]}
+            style={{ width: "clamp(100px, 20vw, 200px" }}
+          >
+            {value}
+          </p>
+        ),
+      },
     ],
     []
   );
@@ -36,33 +69,35 @@ const CommitTable: React.FC<CommitTableProps> = ({ data }) => {
     });
 
   return (
-    <table {...getTableProps()} style={{ border: "1px solid black" }}>
-      <thead>
-        {headerGroups.map((headerGroup: HeaderGroup<Commit>) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column: Column<Commit>) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row: Row<Commit>) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell: Cell<Commit>) => {
-                return (
-                  <td {...cell.getCellProps()} style={{ padding: "10px" }}>
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
+    <div className={classes.container}>
+      <table {...getTableProps()} className={classes.table}>
+        <thead className={classes.header}>
+          {headerGroups.map((headerGroup: HeaderGroup<Commit>) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column: Column<Commit>) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className={classes.body}>
+          {rows.map((row: Row<Commit>) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} className={classes.row}>
+                {row.cells.map((cell: Cell<Commit>) => {
+                  return (
+                    <td {...cell.getCellProps()} className={classes.field}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
