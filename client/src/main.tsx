3e5601527273549c58dd08940effab8079761f8b
Alvaro Clavijo <clavibaldi@gmail.com>
2023-10-08 20:27:36 -0400

    [THF-7] Make default commits page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import CommitsPage from "./pages/Commits/index.tsx";
 
 import "./index.css";
 import "./styles/reset-styles.css";
 import "./styles/fonts.css";
 
+import { ROUTES } from "./config/routes";
+
 const router = createBrowserRouter([
   {
-    path: "/commits",
+    path: ROUTES.COMMITS,
     element: <CommitsPage />,
+  },
+  {
+    path: "*",
+    element: <Navigate to={ROUTES.COMMITS} replace />,
   },
 ]);
 
